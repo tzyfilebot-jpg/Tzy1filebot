87be95c98641840e18ed0a97c9ecd386251f80e0
tzyfilebot-jpg <tzyfilebot@gmail.com>
2026-05-30 20:40:07 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,13 @@ import os
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
 DB_CHANNEL_ID = int(os.getenv("DB_CHANNEL_ID"))
+
 OWNER_ID = int(os.getenv("OWNER_ID"))
+
+UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL")
+NOTIF_CHANNEL = os.getenv("NOTIF_CHANNEL")
