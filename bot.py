2ff7f885117357eb3ff9e1d2b7bee74a52a13b65
tzyfilebot-jpg <tzyfilebot@gmail.com>
2026-06-02 15:57:54 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,10 +31,14 @@ from aiogram.exceptions import (
 # LOGGING CONFIG
 # =========================
 
+import logging
+
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s | %(levelname)s | %(name)s | %(message)s"
 )
+
+logger = logging.getLogger(__name__)
 # =========================
 # CONFIG
 # =========================
