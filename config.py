dd302030b74c85fde1b66913d599eb15a7a27c75
tzyfilebot-jpg <tzyfilebot@gmail.com>
2026-06-04 02:23:52 +0700

    Update config.py

--- config.py
+++ config.py
@@ -14,8 +14,8 @@ ADMINS = set(
     int(x) for x in os.getenv("ADMINS", "").split(",") if x.strip().isdigit()
 )
 
-FORCE_CHANNEL = os.getenv("FORCE_CHANNEL")
+FORCE_CHANNEL = int(os.getenv("FORCE_CHANNEL")
 UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL")
-NOTIFICATION_CHANNEL = os.getenv("NOTIFICATION_CHANNEL")
+NOTIFICATION_CHANNEL = int(os.getenv("NOTIFICATION_CHANNEL")
 
 VIP_LINK = os.getenv("VIP_LINK")
